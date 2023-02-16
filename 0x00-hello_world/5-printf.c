9342e369c69827ab1218dbb386d1fa4158e243ca
harrixoxo <harrixo@gmail.com>
2023-02-16 15:29:13 -0800

    Printf again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - this uses printf to print the main function
  *
- * return 0 (success)
+ * return - returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
