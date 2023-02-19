05919075fc39cdb2508d41d0c419001ef4385d5c
harrixoxo <harrixo@gmail.com>
2023-02-19 14:24:55 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all single digit numbers of base 10 starting
  * from 0, followed by a new line.
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
