b86e33d42ba8f3ae7b9bf3114623637a77dc2fe4
harrixoxo <harrixo@gmail.com>
2023-02-19 12:06:56 -0800

    UNIX printing

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Write a C program that prints exactly and that
+ * piece of art is useful.
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
